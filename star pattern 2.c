53ac9bea0722e9c72db9a19b0134a769a7a488da
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-10-01 18:43:04 +0500

    two different techniques

---
+++ star pattern 2.c
@@ -0,0 +1,35 @@
+main()
+{
+    int i,j;
+
+    for (i=1;i<=5;i++)
+    {
+        for (j=1;j<=5;j++)
+        {
+              /*
+               if(j<=5-i)
+               {
+                 printf("   ");
+               }
+               else
+               {
+                printf(" * ");
+               }
+               */
+
+               if(j>=6-i)
+               {
+                   printf(" * ");
+               }
+               else
+               {
+                   printf("   ");
+               }
+
+        }
+        printf("\n");
+
+    }
+
+    getch();
+}
